c8aae54a1af831575894992c1068eea23f869792
aparecido nepomuceno <isacnepomuceno.np@gmail.com>
2022-01-18 17:32:44 -0300

    API

--- src/main/java/com/algaworks/crm/controller/ClienteController.java
+++ src/main/java/com/algaworks/crm/controller/ClienteController.java
@@ -3,8 +3,12 @@ package com.algaworks.crm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.crm.model.Cliente;
@@ -22,4 +26,9 @@ public class ClienteController {
 		return clienteRepository.findAll();
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
 }
